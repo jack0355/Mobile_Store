afeaf808e3cfb054c7bd8a901d6a83d5987fe322
Zain Ramadan <zramadan944@gmail.com>
2026-04-27 02:23:32 -0800

    Initial commit

---
+++ Mobile_Store Buisness/ClsUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+namespace Mobile_Store_Buisness
+{
+    public class ClsUser
+    {
+        public static bool CheckLogin(string Username, string Password)
+        {
+          
+            return ClsUsersData.CheckLogin(Username, Password);
+           
+        }
+
+
+      
+    }
+}
---
+++ Mobile_Store Data Access/ClsUserData.cs
@@ -0,0 +1,35 @@
+﻿using Mobile_Store_Data_Access;
+using System;
+using System.Data.SqlClient;
+
+public class ClsUsersData
+{
+    public static bool CheckLogin(string Username, string Password)
+    {
+        bool isFound = false;
+        SqlConnection connection = new SqlConnection(ClsConnectionString.ConnectionString);
+        string query = @"SELECT * FROM users 
+                        WHERE username = @username 
+                        AND password = @password";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@username", Username);
+        command.Parameters.AddWithValue("@password", Password);
+        try
+        {
+            connection.Open();
+            SqlDataReader Reader = command.ExecuteReader();
+            if (Reader.Read())
+                isFound = true;
+            Reader.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            connection.Close();
+        }
+        return isFound;
+    }
+}
